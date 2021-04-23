9110bc28544676e3b9d25b83e1ab46d33be91340
Shabair <Shabair143@gmail.com>
2021-04-23 23:14:36 +0500

    Almost ready

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,28 +1,79 @@
 import UserModel from '../../models/authSchema.js'
-
+import bcrypt from 'bcrypt'
 
 /**
  * User Signin
  */
 export const signin = async (req, res) => {
+    var passwordCheck = '';
+
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
-        if(!email || !password){
-            
-            return res.status(422).json({error: `Please Enter Username or Password.`})
+        if (!email || !password) {
+
+            return res.status(422).json({ error: `Please Enter Username or Password.` })
 
         }
 
-        const user = await UserModel.findOne({email,password});
+        const user = await UserModel.findOne({ email });
+        if(user)
+            passwordCheck = bcrypt.compareSync(password, user.password)
 
-        console.log(user);
-    
-        if(!user){
-            return res.status(422).json({error: `User Email Or Password Does not exist`})
+        if (!user || !passwordCheck) {
+            return res.status(422).json({ error: `User Email Or Password Does not exist` })
         }
-        
-        res.send(user);
+
+        const token = await user.generateAuthToken();
+
+        res.cookie("jwt_token",token,{
+            expires: new Date(Date.now() + 25892000000),
+            httpOnly:true
+        });
+        res.send(`Toke: ${token}`);
+
+    } catch (error) {
+
+        res.status(500).json({
+
+            error: `"User Registration Failed!" with this error: ${error.message}`
+
+        });
+
+    }
+
+}
+
+/**
+ * User Signup
+ */
+export const signup = async (req, res) => {
+    try {
+        const { name, email, phone, work, password, cpassword } = req.body;
+
+        if (!name || !email || !phone || !work || !password || !cpassword) {
+            return res.status(422).json({ error: "Plz Filled the fields properly" });
+        }
+
+        const emailExist = await UserModel.findOne({ email: email });
+
+        if (emailExist) {
+            return res.status(422).json({ error: "Email Exist! Please Select a New One" });
+        }else if(password !== cpassword){
+            return res.status(422).json({ error: "Your Confirm Password does not match with Password" });
+        }
+
+        const user = new UserModel({ name, email, phone, work, password });
+
+        const newUser = await user.save();
+
+        if (newUser) {
+            return res.status(201).json({
+                message: "User Registered Successfuly.",
+                data: newUser
+            });
+        }
+
 
     } catch (error) {
 
@@ -31,52 +82,5 @@ export const signin = async (req, res) => {
         });
 
     }
-
-
-}
-
-/**
- * User Signup
- */
-export const signup = async (req, res) => {
-
-    const { name, email, phone, work, password, cpassword } = req.body;
-
-    if (!name || !email || !phone || !work || !password || !cpassword) {
-        return res.status(422).json({ error: "Plz Filled the fields properly" });
-    }
-
-    if (password !== cpassword) {
-        return res.status(422).json({ error: "Your Confirm Password does not match with Password" });
-    }
-
-    try {
-        const emailExist = await UserModel.findOne({ email: email });
-        
-        if (emailExist) {
-            return res.status(422).json({ error: "Email Exist! Please Select a New One" });
-        }
-
-        const user = new UserModel({ name, email, phone, work, password });
-
-        const newUser = await user.save();
-
-        console.log(newUser)
-
-        if(newUser){
-            return res.status(201).json({
-                message: "User Registered Successfuly.",
-                data: newUser
-            });
-        }
-
-        
-    } catch{(error) => {
-
-        res.status(500).json({
-            error: `"User Registration Failed!" with this error: ${error.message}`
-        });
-
-    }}
 
 }
--- models/authSchema.js
+++ models/authSchema.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 
 const authSchema = mongoose.Schema(
     {
@@ -32,10 +35,40 @@ const authSchema = mongoose.Schema(
         password: {
             type: String,
             required: true
-        }
+        },
+        tokens: [
+            {
+                token:{
+                    type:String,
+                    required:true
+                }
+            }
+        ]
 
     }, { timestamps: true }
 );
+
+authSchema.pre('save',async function(next){
+
+    if(this.isModified('password')){
+        
+        this.password = await bcrypt.hash(this.password,12);
+    }
+
+    next()
+});
+
+//Auth Token Generation
+authSchema.methods.generateAuthToken = async function(){
+    try {
+        let tokenGen = jwt.sign({_id:this._id},process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:tokenGen});
+        await this.save();
+        return tokenGen;
+    } catch (error) {
+        
+    }
+}
 
 
 const authModel = mongoose.model('user', authSchema);
